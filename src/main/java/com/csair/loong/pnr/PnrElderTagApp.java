3d1d6bc45f1ebf51ba6008fc8e0dc12c173d2c37
cloudoo-pj <cloudoo-pj@DESKTOP-IQNT1S4>
2016-10-01 08:41:51 -0500

    1.增加解压的processor

--- src/main/java/com/csair/loong/pnr/PnrElderTagApp.java
+++ src/main/java/com/csair/loong/pnr/PnrElderTagApp.java
@@ -1,28 +1,82 @@
 package com.csair.loong.pnr;
 
+import com.csair.loong.pnr.processor.PnrDivProcessor;
 import com.csair.loong.pnr.processor.PnrElderTagProcessor;
 import com.csair.loong.pnr.processor.PnrNorTagProcessor;
 import com.csair.loong.pnr.processor.Processor;
+import com.csair.loong.pnr.processor.UnzipProcessor;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by cloudoo on 2016/9/23.
  */
 public class PnrElderTagApp {
-
+	 protected static final Logger log = LoggerFactory.getLogger(PnrElderTagApp.class);
+	    
     public static void main(String[] args){
 
         String orgFileName = "D:\\03_工作文件\\02_研究院\\01_项目\\05_PNR数据\\02_数据\\2015\\eldprocessor\\CZ_DFP_20151019_1.txt";
+        orgFileName = "E:\\10_lsy\\01_workspace\\test";
+        String unzipFileDir = "E:\\10_lsy\\01_workspace\\orgFile";
+        
         Processor<File,File> testP = new PnrNorTagProcessor();
 //        File orgFile = new File(orgFileName);
 //        System.out.print(orgFile.getAbsolutePath());
 //        File resultFile = testP.doit(new File(orgFileName));
-
+        
+        
+        
         Processor<File,File> pnrProcessor = new PnrElderTagProcessor();
+        
+        
+        List<File> srcFile = findZipFile(orgFileName);
+         
+        Processor unzipP = new UnzipProcessor(unzipFileDir);
+        
+        for(File zipFile:srcFile){
+        	List<File> files = (List<File>)unzipP.doit(zipFile.getAbsolutePath());
+        	for(File orgFile:files){
+        		File eldTagFile = pnrProcessor.doit(
+                        testP.doit(orgFile)
+                );
+        		if(!orgFile.delete()){
+        			log.error("删除失败"+orgFile.getName());
+        		}
+        	}
+        	
+        }
 //        orgFileName = "D:\\03_工作文件\\02_研究院\\01_项目\\05_PNR数据\\02_数据\\2015\\eldprocessor\\CZ_DFP_20151019_1.txt.eldertag";
-        File eldTagFile = pnrProcessor.doit(
-                testP.doit(new File(orgFileName))
-        );
+        
+        
+        
+    }
+    
+    
+    
+    public static List<File> findZipFile(String src){
+    	List<File> rsList = new ArrayList<File>();
+    	File file = new File(src);
+    	if(file.isDirectory()){
+    		
+    		File[] tempFile = file.listFiles();
+    		for(File temp:tempFile){
+    			String fileName=temp.getName();
+    		      String prefix=fileName.substring(fileName.lastIndexOf(".")+1);
+    			if(temp.isFile()&&prefix.equalsIgnoreCase("zip")){
+    				rsList.add(temp);
+    			}
+    		}
+
+    	}
+    	
+    	
+    	return rsList;
+    	
     }
 }
---
+++ src/main/java/com/csair/loong/pnr/processor/UnzipProcessor.java
@@ -0,0 +1,80 @@
+package com.csair.loong.pnr.processor;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UnzipProcessor extends Processor<List<File>, String> {
+	protected static final Logger log = LoggerFactory.getLogger(UnzipProcessor.class);
+	private static final int buffer = 2048;
+	
+	private String destPath;
+	public UnzipProcessor(String destPath){
+		this.destPath = destPath;
+	}
+
+	@Override
+	public List<File> doit(String fileName) {
+		List<File> fileList = new ArrayList<File>();
+		File srcFile = new File(fileName);
+		int count = -1;
+		int index = -1;
+
+		
+		if(destPath==null){
+			destPath = srcFile.getParent();
+		}
+
+	
+
+		log.info("开始解压文件" + fileName + "到：" + destPath);
+		
+		try {
+			BufferedOutputStream bos = null;
+			ZipEntry entry = null;
+			FileInputStream fis = new FileInputStream(fileName);
+			ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
+
+			while ((entry = zis.getNextEntry()) != null) {
+				byte data[] = new byte[buffer];
+
+				String temp = entry.getName();
+				index = temp.lastIndexOf("/");
+				if (index > -1)
+					temp = temp.substring(index + 1);
+				temp = destPath + "//" + temp;
+
+				File f = new File(temp);
+				f.createNewFile();
+
+				FileOutputStream fos = new FileOutputStream(f);
+				bos = new BufferedOutputStream(fos, buffer);
+
+				while ((count = zis.read(data, 0, buffer)) != -1) {
+					bos.write(data, 0, count);
+				}
+
+				bos.flush();
+				bos.close();
+				fileList.add(f);
+			}
+
+			zis.close();
+
+		} catch (Exception e) {
+			log.error("文件解压错误！", e);
+		}
+
+		return fileList;
+	}
+
+}
